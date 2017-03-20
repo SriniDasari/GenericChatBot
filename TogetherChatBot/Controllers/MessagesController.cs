ab6ce509a885c15ab5f289084e102b0d509454a0
SriniDasari <sgdasari@gmail.com>
2017-03-20 11:54:34 +0530

    Commit Morrison demo

--- TogetherChatBot/Controllers/MessagesController.cs
+++ TogetherChatBot/Controllers/MessagesController.cs
@@ -10,7 +10,7 @@ using Newtonsoft.Json;
 using TogetherChatBot.Models;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.FormFlow;
-
+using System.Configuration;
 
 namespace TogetherChatBot
 {
@@ -23,57 +23,16 @@ namespace TogetherChatBot
         /// </summary>
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
-            string[] strArray = { "1", "2", "3", "4", "5" };
+            string initiatePOCFor = ConfigurationManager.AppSettings["InitiatePOCFor"].ToString();
             if (activity.Type == ActivityTypes.Message)
             {
-                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
-
-                if ((activity.Text.ToLower().Equals("hi")) || activity.Text.ToLower().Equals("hello"))
+                if (initiatePOCFor.Equals("T"))
                 {
-                    Activity reply = activity.CreateReply("Hello there! How can I help you today?");
-                    await connector.Conversations.ReplyToActivityAsync(reply);
+                    await InitiateTogetherActivity(activity);
                 }
-                else if (activity.Text.ToLower().Contains("ensure") || activity.Text.ToLower().Contains("call"))
+                else if (initiatePOCFor.Equals("M"))
                 {
-                    Activity reply = activity.CreateReply("Noted! Is there anything else I can help you with?");
-                    await connector.Conversations.ReplyToActivityAsync(reply);
-                }
-                else if ((activity.Text.ToLower().Contains("nope")) || activity.Text.ToLower().Contains("ok") || (activity.Text.ToLower().Contains("thanks")) || (activity.Text.ToLower().Contains("thank")) || (activity.Text.ToLower().Contains("nothing")))
-                {
-                    Activity reply = activity.CreateReply("Ok. Have a good day!");
-                    await connector.Conversations.ReplyToActivityAsync(reply);
-                }
-                else if (activity.Text.ToLower().Contains("complaint") || activity.Text.ToLower().Contains("notice") || activity.Text.ToLower().Contains("arrears") || activity.Text.ToLower().Contains("credit") || activity.Text.ToLower().Contains("rating"))
-                {
-                    if (activity.Text.ToLower().Contains("complaint"))
-                    {
-                        Activity reply = activity.CreateReply("Please tell me. How can i help you?");
-                        await connector.Conversations.ReplyToActivityAsync(reply);
-                    }
-                    else if (activity.Text.ToLower().Contains("notice") || activity.Text.ToLower().Contains("arrears") || activity.Text.ToLower().Contains("credit"))
-                    {
-                        if (activity.Text.ToLower().Contains("receive"))
-                        {
-                            Activity reply = activity.CreateReply("We will need to discuss your account in order to answer your query. Find out how to contact us to discuss your account at https://togethermoney.com/get-in-touch/personal-lending/.");
-                            await connector.Conversations.ReplyToActivityAsync(reply);
-                        }
-                        else if (activity.Text.ToLower().Contains("affect") || activity.Text.ToLower().Contains("impact") || activity.Text.ToLower().Contains("credit") || activity.Text.ToLower().Contains("rating"))
-                        {
-                            Activity reply = activity.CreateReply("No, the Notice of Arrears letter simply details any outstanding instalments on your mortgage/loan account, and in itself has no impact on your credit rating. However, having late or missed payments on your mortgage/loan account will affect your credit rating.");
-                            await connector.Conversations.ReplyToActivityAsync(reply);
-                        }
-                    }
-
-                }
-
-                //else if (activity.Text.ToLower().Contains("apply") || activity.Text.ToLower().Contains("email") || activity.Text.ToLower().Contains("phone") || activity.Text.ToLower().Contains("number") || activity.Text.ToLower().Contains("call") || activity.Text.ToLower().Contains("name"))
-                //{
-                //    // await Conversation.SendAsync(activity, () => new ApplicationDialog());
-                //    await Conversation.SendAsync(activity, MakeAccountDialog);
-                //}
-                else //if (activity.Text.ToLower().Contains("loan") || activity.Text.ToLower().Contains("mortgage") || activity.Text.ToLower().Contains("bridging") || strArray.Any(activity.Text.Equals))
-                {
-                    await Conversation.SendAsync(activity, MakeLoanDialog);
+                    await InitiateMorrisonActivity(activity);
                 }
 
             }
@@ -83,6 +42,93 @@ namespace TogetherChatBot
             }
             var response = Request.CreateResponse(HttpStatusCode.OK);
             return response;
+        }
+
+        private static async Task InitiateTogetherActivity(Activity activity)
+        {
+            string[] strArray = { "1", "2", "3", "4", "5" };
+            ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+            if ((activity.Text.ToLower().Equals("hi")) || activity.Text.ToLower().Equals("hello"))
+            {
+                Activity reply = activity.CreateReply("Hello there! How can I help you today?");
+                await connector.Conversations.ReplyToActivityAsync(reply);
+            }
+            else if (activity.Text.ToLower().Contains("ensure") || activity.Text.ToLower().Contains("call"))
+            {
+                Activity reply = activity.CreateReply("Noted! Is there anything else I can help you with?");
+                await connector.Conversations.ReplyToActivityAsync(reply);
+            }
+            else if ((activity.Text.ToLower().Contains("nope")) || activity.Text.ToLower().Contains("ok") || (activity.Text.ToLower().Contains("thanks")) || (activity.Text.ToLower().Contains("thank")) || (activity.Text.ToLower().Contains("nothing")))
+            {
+                Activity reply = activity.CreateReply("Ok. Have a good day!");
+                await connector.Conversations.ReplyToActivityAsync(reply);
+            }
+            else if (activity.Text.ToLower().Contains("complaint") || activity.Text.ToLower().Contains("notice") || activity.Text.ToLower().Contains("arrears") || activity.Text.ToLower().Contains("credit") || activity.Text.ToLower().Contains("rating"))
+            {
+                if (activity.Text.ToLower().Contains("complaint"))
+                {
+                    Activity reply = activity.CreateReply("Please tell me. How can i help you?");
+                    await connector.Conversations.ReplyToActivityAsync(reply);
+                }
+                else if (activity.Text.ToLower().Contains("notice") || activity.Text.ToLower().Contains("arrears") || activity.Text.ToLower().Contains("credit"))
+                {
+                    if (activity.Text.ToLower().Contains("receive"))
+                    {
+                        Activity reply = activity.CreateReply("We will need to discuss your account in order to answer your query. Find out how to contact us to discuss your account at https://togethermoney.com/get-in-touch/personal-lending/.");
+                        await connector.Conversations.ReplyToActivityAsync(reply);
+                    }
+                    else if (activity.Text.ToLower().Contains("affect") || activity.Text.ToLower().Contains("impact") || activity.Text.ToLower().Contains("credit") || activity.Text.ToLower().Contains("rating"))
+                    {
+                        Activity reply = activity.CreateReply("No, the Notice of Arrears letter simply details any outstanding instalments on your mortgage/loan account, and in itself has no impact on your credit rating. However, having late or missed payments on your mortgage/loan account will affect your credit rating.");
+                        await connector.Conversations.ReplyToActivityAsync(reply);
+                    }
+                }
+
+            }
+
+            //else if (activity.Text.ToLower().Contains("apply") || activity.Text.ToLower().Contains("email") || activity.Text.ToLower().Contains("phone") || activity.Text.ToLower().Contains("number") || activity.Text.ToLower().Contains("call") || activity.Text.ToLower().Contains("name"))
+            //{
+            //    // await Conversation.SendAsync(activity, () => new ApplicationDialog());
+            //    await Conversation.SendAsync(activity, MakeAccountDialog);
+            //}
+            else //if (activity.Text.ToLower().Contains("loan") || activity.Text.ToLower().Contains("mortgage") || activity.Text.ToLower().Contains("bridging") || strArray.Any(activity.Text.Equals))
+            {
+                await Conversation.SendAsync(activity, MakeLoanDialog);
+            }
+        }
+
+        private static async Task InitiateMorrisonActivity(Activity activity)
+        {
+
+            ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+            if ((activity.Text.ToLower().Equals("hi")) || activity.Text.ToLower().Equals("hello"))
+            {
+                Activity reply = activity.CreateReply("Hello there! How can I help you today?");
+                await connector.Conversations.ReplyToActivityAsync(reply);
+            }
+            else if (activity.Text.ToLower().Contains("ingredients") || activity.Text.ToLower().Contains("pudding"))
+            {
+                Activity reply = activity.CreateReply("Sure. Can I have your name please?");
+                await connector.Conversations.ReplyToActivityAsync(reply);
+            }
+            else if (activity.Text.ToLower().Contains("name"))
+            {
+                Activity reply = activity.CreateReply("Thanks. Please find below the ingredients required. 140g plain flour,4 eggs,200 ml milk, sunflower oil for 8 large puds. Would you like to add the items to your cart?");                
+                await connector.Conversations.ReplyToActivityAsync(reply);
+            }          
+            else if ((activity.Text.ToLower().Contains("yes")) || activity.Text.ToLower().Contains("please"))
+            {
+                Activity reply = activity.CreateReply("Items added to your cart. Please login to checkout. Please rate your experience with us here");
+                await connector.Conversations.ReplyToActivityAsync(reply);
+            }
+            else if ((activity.Text.ToLower().Contains("thanks")) || (activity.Text.ToLower().Contains("thank")) || (activity.Text.ToLower().Contains("ok")))
+            {
+                Activity reply = activity.CreateReply("Ok. Have a good day!");
+                await connector.Conversations.ReplyToActivityAsync(reply);
+            }
+
         }
 
         private Activity HandleSystemMessage(Activity message)
